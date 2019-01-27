e76ccc8b28ca943014dd59dfc2eb3d69e6db4a14
RoyaL.Sanchez <42938856+RoyaLSanchez@users.noreply.github.com>
2019-01-26 20:46:25 -0800

    RemoveFrobidden

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,4 @@
 <div class="page-footer">
-    <div class="page-footer-inner"> 2017 &copy; Froiden
-      
-    </div>
     <div class="scroll-to-top">
         <i class="icon-arrow-up"></i>
     </div>
